aefbfe14c7afcac7e477f183ccef71bb7704b6b2
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-12-01 06:49:46 +0100

    Day 01 - TypeScript

--- typescript/day-01/part-1.ts
+++ typescript/day-01/part-1.ts
@@ -1,5 +1,17 @@
-import { loadInput } from "../utils";
+import { loadInput, sum, toInt } from "../utils";
 
 const input = loadInput();
 
-console.log(input);
+const list = input.split("\n").map((line) => {
+  const [x, y] = line.split(" ".repeat(3));
+  return { x: toInt(x), y: toInt(y) };
+});
+
+const distance = (a: number, b: number) => Math.abs(a - b);
+
+const xs = list.map((line) => line.x).toSorted();
+const ys = list.map((line) => line.y).toSorted();
+
+const distances = xs.map((x, i) => distance(x, ys[i]));
+
+console.log(sum(distances));
--- typescript/day-01/part-2.ts
+++ typescript/day-01/part-2.ts
@@ -1,5 +1,17 @@
-import { loadInput } from "../utils";
+import { loadInput, sum, toInt } from "../utils";
 
 const input = loadInput();
 
-console.log(input);
+const list = input.split("\n").map((line) => {
+  const [x, y] = line.split(" ".repeat(3));
+  return { x: toInt(x), y: toInt(y) };
+});
+
+const xs = list.map((line) => line.x).toSorted();
+const ys = list.map((line) => line.y).toSorted();
+
+const xsCountMultiplied = xs.map((x) => x * ys.filter((y) => y === x).length);
+
+const similarityScore = sum(xsCountMultiplied);
+
+console.log(similarityScore);
